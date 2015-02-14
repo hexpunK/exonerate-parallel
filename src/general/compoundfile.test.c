d8df9abd91bc521dd655a7144f6b6ac8ed716d74
Jordan Woerner <jordanw2004@gmail.com>
2015-02-14 00:29:11 +0000

    Placed globals.h in a better place.

--- src/general/compoundfile.test.c
+++ src/general/compoundfile.test.c
@@ -14,6 +14,7 @@
 \****************************************************************/
 
 #include <stdio.h>
+
 #include "globals.h"
 #include "compoundfile.h"
 
--- src/general/globals.h
+++ src/general/globals.h
@@ -1,6 +1,8 @@
 #ifndef __EXONERATE_GLOBALS
 #define __EXONERATE_GLOBALS
 
+#include <stdio.h>
+
 extern FILE *file;
 
 #endif // __EXONERATE_GLOBALS
